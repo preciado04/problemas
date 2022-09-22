5360b2478baca0407cbbeafb689e069cb1df4928
Abraham Preciado Fonseca <preciado0425@gmail.com>
2022-09-22 11:07:27 -0600

    Initial commit.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * This is scripts.js file.
+ */
+
+(function ($) {
+  'use strict';
+
+  $(document).on('submit', 'form#largest-substring-size', function (e) {
+    e.preventDefault();
+  });
+
+})(jQuery);
